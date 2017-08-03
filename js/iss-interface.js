216f878e1b5a8328ee5e3af07c064afd121726da
Emilie Thoreson and Saul Artiga <emilie+saul@>
2017-08-03 11:27:20 -0700

    add setInterval function

--- js/iss-interface.js
+++ js/iss-interface.js
@@ -2,14 +2,9 @@ var Iss = require('./../js/iss.js').IssModule;
 $(document).ready(function() {
   var newIss = new Iss();
   function displayIss(lat, lon) {
-    $("#iss-now").append("Latitude: " + lat + ", Longitude: " + lon);
+    $("#iss-now").text("Latitude: " + lat + ", Longitude: " + lon);
   }
 
    newIss.getLocation(displayIss);
-
-  // setTimeout(function(){
-  //   while (true){
-  //     newIss.getLocation(displayIss);
-  //   }
-  // }, 5);
+   setInterval(function() { newIss.getLocation(displayIss); }, 5000);
 });
