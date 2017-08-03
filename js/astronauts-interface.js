67a16f6f57ce2b1bb3b160e2355e4c33553bddf3
Emilie Thoreson and Saul Artiga <emilie+saul@>
2017-08-03 11:11:29 -0700

    add js and css to improve ui and site navigation

--- js/astronauts-interface.js
+++ js/astronauts-interface.js
@@ -1,16 +1,28 @@
 var Astronauts = require('./../js/astronauts.js').AstronautsModule;
 
 $(document).ready(function(){
-  var displayAstronauts = function(name, ship, total){
+
+  var displayTotal = function(total){
+    $('#total-astros').text("There are currently " + total + " astronauts in space:");
+  };
+
+  var displayAstronauts = function(name, ship){
+    var nameArray = name.split(" ");
+    console.log(nameArray[1]);
     $('.astros-in-space').append("<div class = 'astronaut'>" +
     "<img src = '../../images/spaceman.png'>" +
-    "<h3>" +
+    "<span class='dont-move'><h3><a href='//en.wikipedia.org/wiki/" +
+    nameArray[0] +
+    "_" +
+    nameArray[1] +
+    "'>" +
     name +
-    "</h3>" +
+    "</a></h3>" +
     "<h4>Currently aboard the " +
     ship +
-    "</h4></div>");
+    "</h4></span></div>");
   };
   var astro = new Astronauts();
+  astro.displayTotalAstros(displayTotal);
   astro.getResults(displayAstronauts);
 });
--- js/astronauts.js
+++ js/astronauts.js
@@ -1,11 +1,16 @@
 function Astronauts() {
 }
 
+Astronauts.prototype.displayTotalAstros = function(displayTotal) {
+  $.get("http://api.open-notify.org/astros.json").then(function(response) {
+    displayTotal(response.number);
+  });
+};
+
 Astronauts.prototype.getResults = function(displayAstronauts) {
   $.get("http://api.open-notify.org/astros.json").then(function(response) {
-    console.log(JSON.stringify(response));
     for(var i = 0; i < response.number; i++) {
-      displayAstronauts(response.people[i].name, response.people[i].craft, response.number);
+      displayAstronauts(response.people[i].name, response.people[i].craft);
     }
   });
 };
